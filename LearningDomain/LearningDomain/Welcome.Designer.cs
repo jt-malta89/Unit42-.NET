4dcfd57c8e90cbda461e05cb4bb34a5c9cf8db53
jt-malta89 <tedescojaems@gmail.com>
2019-02-16 12:09:04 +0100

    Changed Form Size

--- LearningDomain/LearningDomain/Welcome.Designer.cs
+++ LearningDomain/LearningDomain/Welcome.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(509, 441);
             this.Name = "Welcome";
             this.Text = "Welcome";
             this.ResumeLayout(false);
