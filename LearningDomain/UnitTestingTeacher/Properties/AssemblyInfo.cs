9752879bb7d833131d701038bc148195428c3ce2
jt-malta89 <tedescojaems@gmail.com>
2019-03-06 01:09:12 +0100

    Unit Testing 2

---
+++ LearningDomain/UnitTestingTeacher/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestingTeacher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestingTeacher")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b9f6771-27e7-428a-95a3-bb6d98a12869")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LearningDomain/UnitTestingTeacher/UnitTest2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ApplicationLogic;
+using ObjectGettersSetters;
+
+namespace UnitTestingTeacher
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestSaveTeacher_Fails()
+        {
+            Teacher teacher = new Teacher();
+            bool resSaveTeacher = TeacherService.Save(teacher);
+            Assert.IsFalse(resSaveTeacher);
+        }
+
+        [TestMethod]
+        public void TestSaveTeacher_Succeeds()
+        {
+            Teacher teacher = new Teacher();
+            teacher.IdCard = "59780M";
+            bool resSaveTeacher = TeacherService.Save(teacher);
+            Assert.IsTrue(resSaveTeacher);
+        }
+
+
+        [TestMethod]
+        public void TestLoadTeacher_Fails()
+        {
+            Teacher teacher = TeacherService.Load("59780");
+            Assert.IsNull(teacher);
+        }
+
+        [TestMethod]
+        public void TestLoadTeacher_Succeeds()
+        {
+
+            Teacher teacher = TeacherService.Load("59780M");
+            Assert.IsNotNull(teacher);
+            Assert.AreEqual(teacher.IdCard, "59780M");
+        }
+
+
+        [TestMethod]
+        public void TestLoadTeachers_Succeeds()
+        {
+
+            List<Teacher> lstTeachers = TeacherService.Load();
+            Assert.IsNotNull(lstTeachers);
+            Assert.IsTrue(lstTeachers.Count > 0);
+            Assert.IsNotNull(lstTeachers[0]);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(lstTeachers[0].IdCard));
+        }
+    }
+}
